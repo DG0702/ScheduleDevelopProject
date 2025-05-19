8d9bb5de4f9602ea2b0eb9e6a657a0ee89577c2f
DongGeun <kk506kk@naver.com>
2025-05-19 14:45:05 +0900

    jpa 연결과 mysql 테이블 생성, 테이블 생성 코드 입력

---
+++ schedule.sql
@@ -0,0 +1,28 @@
+create database scd;
+show databases;
+use scd;
+
+create table user (
+                      user_id bigint auto_increment primary key not null,
+                      user_name varchar(4) not null,
+                      user_email varchar(20) not null,
+                      password varchar(10) not null,
+                      created_at timestamp not null,
+                      updated_at timestamp not null
+);
+
+
+create table schedule (
+                          schedule_id bigint auto_increment primary key not null,
+                          user_id bigint not null,
+                          password varchar(10) not null,
+                          title varchar(10) not null,
+                          contents varchar(100),
+                          created_date timestamp not null ,
+                          updated_date timestamp not null ,
+                          foreign key (user_id) references user (user_id)
+);
+
+show tables;
+desc user;
+desc schedule;
