417e31dad54616d713150e8da6accac2353a4054
DongGeun <kk506kk@naver.com>
2025-05-19 10:49:24 +0900

    init commit

---
+++ src/main/java/com/project/scheduledelevopproject/ScheduleDelevopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.scheduledelevopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDelevopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleDelevopProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/scheduledelevopproject/ScheduleDelevopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.scheduledelevopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDelevopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
