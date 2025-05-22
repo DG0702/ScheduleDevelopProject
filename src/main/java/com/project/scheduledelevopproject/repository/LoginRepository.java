664ee7674497e2e8c6e242fd1bf1fcb61890dac5
DongGeun <kk506kk@naver.com>
2025-05-22 20:12:39 +0900

    Optional 이름 변경

--- src/main/java/com/project/scheduledelevopproject/repository/LoginRepository.java
+++ src/main/java/com/project/scheduledelevopproject/repository/LoginRepository.java
@@ -9,10 +9,10 @@ import java.util.Optional;
 
 public interface LoginRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findOptionalByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     default User findByUserEmail(String email){
-        return findOptionalByEmail(email).orElseThrow(
+        return findByEmail(email).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED,"존재하지 않은 이메일 입니다."));
     }
 }
